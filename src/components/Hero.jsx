c0f8927138bbcabae1a57d81ae0440a235322c43
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-26 19:52:33 +0300

    Implement properties section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ function Hero({ scrollToProperties }) {
   const background = {
     backgroundImage: `url(${Background})`,
     backgroundSize: "cover",
+    backgroundPosition: "center",
   };
   return (
     <>
@@ -17,10 +18,7 @@ function Hero({ scrollToProperties }) {
         <p className="text-sm text-center py-4 uppercase font-light tracking-[0.5em] drop-shadow-md">
           revolutionizing real estate
         </p>
-        <button
-          onClick={scrollToProperties}
-          className="w-48 py-3 my-1 rounded-md shadow-md font-medium bg-white/40 uppercase hover:bg-white/60 hover:shadow-lg focus:bg-white/60 focus:shadow-lg active:scale-95 active:shadow-sm duration-300"
-        >
+        <button onClick={scrollToProperties} className="glass-button">
           discover
         </button>
       </div>
--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,10 +1,78 @@
+import React, { useState } from "react";
+import House1 from "../assets/house1.webp";
+import House2 from "../assets/house2.webp";
+import House3 from "../assets/house3.webp";
+
+const houses = [
+  {
+    src: House1,
+    title: "Rural Villa",
+    description:
+      "Charming rural villa surrounded by lush greenery, offering a peaceful retreat from the bustling city life.",
+  },
+  {
+    src: House2,
+    title: "Modern House",
+    description: "Beautiful modern house with all the amenities.",
+  },
+  {
+    src: House3,
+    title: "Beach House",
+    description:
+      "A stunning beach house with a breathtaking view of the ocean.",
+  },
+];
+
 function Properties({ heroRef }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const currentHouse = houses[currentIndex];
+  const currentBgImage = {
+    backgroundImage: `url(${currentHouse.src})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % houses.length);
+  };
+
+  const handleBack = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + houses.length) % houses.length
+    );
+  };
+
   return (
-    <>
-      <div ref={heroRef} className="custom-container">
-        <h1>hai</h1>
+    <div
+      style={currentBgImage}
+      ref={heroRef}
+      className="w-full min-h-screen flex flex-col justify-center items-center"
+    >
+      <div className="w-full min-h-screen bg-black/50 backdrop-blur-xl flex flex-col justify-center items-center">
+        <div className="w-3/4 flex flex-col justify-center items-center px-4 py-4">
+          <img
+            src={currentHouse.src}
+            alt="Real estate house"
+            className="w-[40rem] rounded-md shadow-md aspect-[4/3]"
+          />
+          <div className="w-full flex flex-col justify-center items-center text-center py-4 text-neutral-200">
+            <h1 className="text-2xl font-medium drop-shadow-md">
+              {currentHouse.title}
+            </h1>
+            <p className="drop-shadow-md py-1">{currentHouse.description}</p>
+          </div>
+          <div className="w-full flex flex-row gap-4 justify-center items-center px-4 py-4 text-neutral-200">
+            <button onClick={handleBack} className="glass-button">
+              back
+            </button>
+            <button onClick={handleNext} className="glass-button">
+              next
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
